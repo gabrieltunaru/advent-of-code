35764692acfeab2dac5eef4953e0a61d5dd85c4f
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2022-12-08 15:30:01 +0200

    Day 7: fix path generation

--- src/main/scala/Day7_1.scala
+++ src/main/scala/Day7_1.scala
@@ -19,6 +19,9 @@ object Day7_1 {
   val index = 7
   val filePath = s"$resources/day_$index.txt"
 
+  def createPathString(pwd: mutable.Stack[String], name: String) =
+    pwd.reverse.toList.appended(name).mkString("/","/","")
+
   def firstPass(lines: List[String], treeList: List[Tree], pwd: mutable.Stack[String]): List[Tree] =
 
     val cd = "\\$ cd (.*)".r
@@ -30,34 +33,40 @@ object Day7_1 {
 
     line
       .map {
-        case cd("/")  => firstPass(lines.tail, treeList, mutable.Stack.from(List("/")))
+        case cd("/")  => firstPass(lines.tail, treeList, mutable.Stack.from(List.empty[String]))
         case cd("..") => firstPass(lines.tail, treeList, pwd.tail)
         case cd(d)    => firstPass(lines.tail, treeList, pwd.push(d))
         case ls()     => firstPass(lines.tail, treeList, pwd)
-        case dir(d)   => firstPass(lines.tail, treeList.appended(Branch(d, pwd.toList)), pwd)
+        case dir(name)   => firstPass(lines.tail, treeList.appended(Branch(createPathString(pwd, name), pwd.toList)), pwd)
         case file(size, name) =>
-          firstPass(lines.tail, treeList.appended(Leaf(name, size.toInt, pwd.toList)), pwd)
+          firstPass(lines.tail, treeList.appended(Leaf(createPathString(pwd, name), size.toInt, pwd.toList)), pwd)
       }
       .getOrElse(treeList)
 
   def updateParents(size: Int, parents: List[String], initial: Map[String, FileInfo]): Map[String, FileInfo] =
     parents.foldLeft(initial)((acc, el) =>
-      val oldSize = acc.getOrElse(el, FileInfo(FileType.Dir, 0)).size
-      acc.updated(el, FileInfo(FileType.Dir, oldSize + size))
+      val index = parents.indexOf(el)
+      val slice = parents.slice(index+1, parents.size)
+      val path = createPathString(mutable.Stack.from(slice), el)
+      val oldSize = acc.getOrElse(path, FileInfo(FileType.Dir, 0)).size
+      acc.updated(path, FileInfo(FileType.Dir, oldSize + size))
     )
 
   def main(args: Array[String]): Unit =
     val fileContents = Source.fromFile(filePath).getLines()
     val root: Tree = Branch("/", Nil)
-    val currentDir = mutable.Stack.from(List("/"))
+    val currentDir = mutable.Stack.from(List.empty[String])
     val treeList = firstPass(fileContents.toList, List(root), currentDir)
     val sizeMap = treeList.foldLeft(Map.empty[String, FileInfo])((acc, el) =>
       el match
         case Branch(name, _)           => acc.updated(name, FileInfo(FileType.Dir, 0))
         case Leaf(name, size, parents) => updateParents(size, parents, acc.updated(name, FileInfo(FileType.File, size)))
     )
-    val dirsLessThan100k = sizeMap.filter((_, fileInfo) => fileInfo.size <= 100000 && fileInfo.fileType == FileType.Dir)
+    val dirsLessThan100k = sizeMap.filter((_, fileInfo) => fileInfo.size < 100000 && fileInfo.fileType == FileType.Dir)
     val sumOfSmallDirs = dirsLessThan100k.foldLeft(0)((acc, el) => acc + el._2.size)
-    println(dirsLessThan100k)
+//    println(dirsLessThan100k)
     println(sumOfSmallDirs)
+    sizeMap.toList.filter(_._2.fileType == FileType.Dir).sorted((a, b) => a._1.compareTo(b._1)).foreach(println)
+    sizeMap.toList.filter(_._2.fileType == FileType.File).sorted((a, b) => a._1.compareTo(b._1)).foreach(println)
+
 }
