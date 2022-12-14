1be54786c2f0b02249bb8488f503d36c49108d26
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2022-12-14 16:42:48 +0200

    Day 12: part 1

--- src/main/scala/Day_12.scala
+++ src/main/scala/Day_12.scala
@@ -2,11 +2,18 @@ package com.cannondev.advent
 
 import util.FileReader
 
+import scala.collection.mutable
 import scala.io.Source
 
 object Day_12:
   case class Point(x: Int, y: Int)
   type Something = List[List[Int]]
+  case class Graph(nodes: Set[Point], edges: List[(Point, Point)]):
+    self =>
+    def ++(graph: Graph): Graph = Graph(self.nodes ++ graph.nodes, self.edges ++ graph.edges)
+
+  object Graph:
+    def empty = Graph(Set.empty, List.empty)
 
   val index = 12
 
@@ -19,61 +26,69 @@ object Day_12:
   def isValid(curr: Point, next: Point, matrix: List[List[Char]]): Boolean =
     val currElevation = getElevation(curr, matrix)
     val nextElevation = getElevation(next, matrix)
-    val heightDiff = nextElevation - currElevation
-    heightDiff == 0 || heightDiff == 1
+    val res = nextElevation <= currElevation || currElevation + 1 == nextElevation
+    println(s"${matrix(curr.x)(curr.y)} ${matrix(next.x)(next.y)} $res")
+    res
 
-  def backtracking(
-      curr: Point,
-      matrix: List[List[Char]],
-      result: List[Char],
-      visited: List[List[Boolean]],
-      allResults: List[List[Char]]
-  ): List[List[Char]] =
-    val x = curr.x
-    val y = curr.y
-    if (visited(x)(y)) return Nil
-    if (matrix(x)(y) == 'E') return result.appended('E') :: allResults
-    println(curr)
 
-    // speedup
-    val maybeMin = allResults.map(_.size).minOption
-    if (maybeMin.isDefined)
-      if (result.size >= allResults.map(_.size).min) return Nil
+  def convertToGraph(
+      matrix: List[List[Char]]
+  ): Graph =
 
-//    debugging.foreach(l => println(l.mkString))
-//    println("")
-    val nextResult = result.appended(matrix(x)(y))
-    val nextVisited = visited.updated(x, visited(x).updated(y, true))
-    val nextPoints = Point(x + 1, y) :: Point(x, y + 1) :: Point(x - 1, y) :: Point(x, y - 1) :: Nil
-    val filteredNextPoints = nextPoints
-      .filter(p => p.x >= 0 && p.y >= 0 && p.x < matrix.size && p.y < matrix.head.size)
-      .filter(p => !visited(p.x)(p.y))
-      .filter(p => isValid(curr, p, matrix))
-//    if (x == 2) {
-//      println("test")
-//    }
-    val nextResults = filteredNextPoints.map(p => backtracking(p, matrix, nextResult, nextVisited, allResults))
+    val nodes = for
+      i <- matrix.indices
+      j <- matrix.head.indices
+    yield Point(i, j)
 
-    nextResults.flatten ++ allResults
+    val edgesMap = for
+      Point(x, y) <- nodes
+      nextPoints = Point(x + 1, y) :: Point(x, y + 1) :: Point(x - 1, y) :: Point(x, y - 1) :: Nil
+      filteredNextPoints = nextPoints
+        .filter(p => p.x >= 0 && p.y >= 0 && p.x < matrix.size && p.y < matrix.head.size)
+        .filter(p => isValid(Point(x, y), p, matrix))
+    yield (Point(x, y) -> filteredNextPoints)
+
+    val edges = edgesMap.flatMap((k, v) => v.map((k, _)))
+
+    Graph(nodes.toSet, edges.toList)
+
+  def isDistanceSmaller(source: Option[Int], target: Option[Int]): Boolean =
+    (source, target) match
+      case (None, None)       => false
+      case (Some(_), None)    => true
+      case (None, Some(_))    => false
+      case (Some(s), Some(t)) => s + 1 < t
+
+  def BellmanFord(graph: Graph, start: Point) =
+    val distance: mutable.Map[Point, Int] = mutable.Map.empty
+    val predecessor: mutable.Map[Point, Point] = mutable.Map.empty
+    distance(start) = 0
+
+    for (_ <- 0 to graph.edges.size)
+      graph.edges.foreach((u, v) =>
+        if (isDistanceSmaller(distance.get(u), distance.get(v)))
+          distance(v) = distance(u) + 1
+          predecessor(v) = u
+      )
+    (distance, predecessor)
 
   def parse(lines: List[String]): List[List[Char]] =
     lines.foldLeft(List.empty[List[Char]])((acc, el) => acc.appended(el.toCharArray.toList))
 
+  def getPosition(c: Char, matrix: List[List[Char]]): Point =
+    val x = matrix.indexWhere(_.contains(c))
+    val y = matrix(x).indexOf(c)
+    Point(x, y)
+
   def main(args: Array[String]): Unit =
     val input = FileReader.readLines(index)
     val parsed = parse(input)
-    val startX = parsed.indexWhere(_.contains('S'))
-    val startY = parsed(startX).indexOf('S')
-    val start = Point(startX, startY)
-    val solutions = backtracking(
-      start,
-      parsed,
-      List.empty,
-      parsed.map(_ => List.fill(parsed.head.size)(false)),
-      List.empty
-    )
-    val min = solutions.map(_.size).min
-//    solutions.foreach(s => println(s"${s.size}: $s"))
-//    println(solutions.map(_.size))
-//    println(solutions.find(_.size==min))
-    println(min)
+    val graph = convertToGraph(parsed)
+    println(graph.nodes.size)
+    val start = getPosition('S', parsed)
+    val end = getPosition('E', parsed)
+    println(start)
+    val (distance, predecessor) = BellmanFord(graph, start)
+
+
+    println(distance(end))
