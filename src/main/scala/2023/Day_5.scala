2c61f1f8179a0ba40e6b67d67d791c7736a142e2
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2023-12-06 00:19:55 +0200

    2023 day 5 part 1 wip

---
+++ src/main/scala/2023/Day_5.scala
@@ -0,0 +1,45 @@
+package com.cannondev.advent
+
+import util.FileReader
+
+import scala.io.Source
+
+object Day_5:
+  case class Input(seeds: List[Int], maps: List[List[PlantingMap]])
+  case class PlantingMap(destinationRange: Int, sourceRange: Int, rangeLength: Int)
+
+  val index = 5
+
+  def parse(lines: String): Input =
+
+    val separated =
+      lines
+        .filter(c => c == ' ' || c == '\n' || c.isDigit)
+        .split("\n\n")
+
+    val seeds = separated.head.split(' ').filter(_.nonEmpty).map(_.toInt)
+    val maps = separated.tail
+      .map(_.split("\n").filter(_.nonEmpty).map(_.split(' ').map(_.toInt)))
+      .map(_.filter(_.nonEmpty).toList.map {
+        case Array(a, b, c) => PlantingMap(a, b, c)
+        case _              => throw new Error("could not match")
+      })
+      .toList
+
+    Input(seeds.toList, maps)
+
+  def part1(input: Input): Int =
+    input.maps.foldLeft(input.seeds)((acc, maps) => {
+      val res = acc.map(seed =>
+        maps.find(map => seed >= map.sourceRange && seed < map.sourceRange + map.rangeLength) match
+          case Some(foundMap) => seed - foundMap.sourceRange + foundMap.destinationRange
+          case None           => seed
+      )
+//      println(res)
+      res
+    }).min
+  def main(args: Array[String]): Unit =
+    val input = FileReader.readString(index, 2023)
+    val parsed = parse(input)
+    println(parsed)
+    println(part1(parsed))
--- src/main/scala/util/FileReader.scala
+++ src/main/scala/util/FileReader.scala
@@ -8,13 +8,13 @@ object FileReader {
   val resources = "src/main/resources"
 
   def readString(index: Int, year: Int = 2022): String =
-    val filePath = s"$resources/day_$index.txt"
+    val filePath = s"$resources/$year/day_$index.txt"
     val source = Source.fromFile(filePath)
     val read = source.mkString
     source.close()
     read
 
-  def readLines(index: Int, year: Int=2022): List[String] =
+  def readLines(index: Int, year: Int = 2022): List[String] =
     val filePath = s"$resources/$year/day_$index.txt"
     val source = Source.fromFile(filePath)
     val read = source.getLines().toList
