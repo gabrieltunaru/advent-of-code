96abcea944e44a9ea85c1d7a0387d8c646f844ab
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2023-12-06 08:48:57 +0200

    2023 day 5 part 2 wip

--- src/main/scala/2023/Day_5.scala
+++ src/main/scala/2023/Day_5.scala
@@ -2,8 +2,6 @@ package com.cannondev.advent
 
 import util.FileReader
 
-import scala.io.Source
-
 object Day_5:
   case class Input(seeds: List[BigInt], maps: List[List[PlantingMap]])
   case class PlantingMap(destinationRange: BigInt, sourceRange: BigInt, rangeLength: BigInt)
@@ -29,17 +27,28 @@ object Day_5:
     Input(seeds.toList, maps)
 
   def part1(input: Input): BigInt =
-    input.maps.foldLeft(input.seeds)((acc, maps) => {
-      val res = acc.map(seed =>
-        maps.find(map => seed >= map.sourceRange && seed < map.sourceRange + map.rangeLength) match
-          case Some(foundMap) => seed - foundMap.sourceRange + foundMap.destinationRange
-          case None           => seed
-      )
-//      println(res)
-      res
-    }).min
+    input.maps
+      .foldLeft(input.seeds)((acc, maps) => {
+        val res = acc.map(seed =>
+          maps.find(map => seed >= map.sourceRange && seed < map.sourceRange + map.rangeLength) match
+            case Some(foundMap) => seed - foundMap.sourceRange + foundMap.destinationRange
+            case None           => seed
+        )
+        res
+      })
+      .min
+
+  def part2(input: Day_5.Input): BigInt =
+    val actualSeeds = input.seeds.grouped(2).toList.flatMap {
+      case a :: b :: Nil => (a to a + b - 1).toList
+      case _             => throw new Error
+    }
+    val actualInput = part1(input.copy(seeds = actualSeeds))
+
+    actualInput
   def main(args: Array[String]): Unit =
     val input = FileReader.readString(index, 2023)
     val parsed = parse(input)
     println(parsed)
     println(part1(parsed))
+    println(part2(parsed))
