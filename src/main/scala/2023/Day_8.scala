84ba71c23913eb8b04540aaeb065bc05bd8be1fd
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2023-12-10 23:42:31 +0200

    2023 day 8 wip

--- src/main/scala/2023/Day_8.scala
+++ src/main/scala/2023/Day_8.scala
@@ -38,8 +38,54 @@ object Day_8:
         case 'R' => map(current).right
       part1(nextStep, end, steps + 1, instructions.tail, map)
 
+  def getStepsToNextZ(
+      current: String,
+      steps: Int,
+      instructions: LazyList[Char],
+      map: Map[String, Directions]
+  ): Int =
+    val instruction = instructions.head
+    if (current.toCharArray.last == 'Z') steps
+    else
+      val nextStep = instruction match
+        case 'L' => map(current).left
+        case 'R' => map(current).right
+      getStepsToNextZ(nextStep, steps + 1, instructions.tail, map)
+
+  def part2(
+      current: List[String],
+      end: List[String],
+      steps: Int,
+      instructions: LazyList[Char],
+      map: Map[String, Directions]
+  ): Int =
+    val instruction = instructions.head
+    if (current.toSet == end.toSet) steps
+    else
+      val nextStep = instruction match
+        case 'L' => current.map(c => map(c).left)
+        case 'R' => current.map(c => map(c).right)
+      part2(nextStep, end, steps + 1, instructions.tail, map)
+
+//  def part2(
+//      current: String,
+//      steps: Int,
+//      instructions: LazyList[Char],
+//      map: Map[String, Directions]
+//  ): Int =
+//    val instruction = instructions.head
+//    if (current.toCharArray.last == 'Z') steps
+//    else
+//      val nextStep = instruction match
+//        case 'L' => map(current).left
+//        case 'R' => map(current).right
+//      part2(nextStep, steps + 1, instructions.tail, map)
+
   def main(args: Array[String]): Unit =
     val input = FileReader.readLines(index, 2023)
     val parsed = parse(input)
     println(parsed)
-    println(part1("AAA", "ZZZ", 0, parsed.instructions, parsed.map))
+//    println(part1("AAA", "ZZZ", 0, parsed.instructions, parsed.map))
+    val start = parsed.map.keys.filter(_.toCharArray.last == 'A').toList
+    val end = parsed.map.keys.filter(_.toCharArray.last == 'Z').toList
+    println(part2(start, end, 0, parsed.instructions, parsed.map))
