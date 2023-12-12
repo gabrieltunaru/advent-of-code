13a430e7d198b2006dec6fa140c59474d6348054
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2023-12-12 22:29:14 +0200

    2023 day 11 part 2

--- src/main/scala/y2023/Day_11.scala
+++ src/main/scala/y2023/Day_11.scala
@@ -5,7 +5,7 @@ import com.cannondev.advent.util.FileReader
 import scala.io.Source
 
 object Day_11:
-  case class Position(line: Int, column: Int)
+  case class Position(line: Long, column: Long)
 
   val index = 11
 
@@ -19,29 +19,37 @@ object Day_11:
     yield Position(index, number.start)
     numbersPositions
 
-  def expandSpace(positions: List[Position]): List[Position] =
-    // gaseste toate liniile goale
-    // gaseste toate coloanele goale
-    // parcurgi listele astea 2 si adaugi spatiu
-    val emptyLines =
-      positions.indices.foldLeft(List.empty[Int])((acc, el) => if (positions.exists(_.line == el)) acc else el :: acc)
-    val emptyColums =
-      positions.indices.foldLeft(List.empty[Int])((acc, el) => if (positions.exists(_.column == el)) acc else el :: acc)
-    val updatedLines = positions.map(p => p.copy(line = p.line + emptyLines.count(_ < p.line)))
-    val updatedLinesAndColumns = updatedLines.map(p => p.copy(column = p.column + emptyColums.count(_ < p.column)))
-    updatedLinesAndColumns
-
-  def part1(positions: List[Position]): Int =
+  def compute(positions: List[Position]): Long =
     val distances = for {
       p1 <- positions
       p2 <- positions
     } yield (p1.line - p2.line).abs + (p1.column - p2.column).abs
     distances.sum / 2
 
+  def expandSpace(positions: List[Position], times: Long, columnCount: Long, lineCount: Long): List[Position] =
+    val emptyLines =
+      (0.toLong until lineCount).foldLeft(List.empty[Long])((acc, el) => if (positions.exists(_.line == el)) acc else el :: acc)
+    val emptyColums =
+      (0.toLong until columnCount).foldLeft(List.empty[Long])((acc, el) =>
+        if (positions.exists(_.column == el)) acc else el :: acc
+      )
+    val updatedLines = positions.map(p =>
+      val emptyLinesCount = emptyLines.count(_ < p.line)
+      p.copy(line = p.line + (emptyLinesCount) * (times - 1))
+    )
+    val updatedLinesAndColumns = updatedLines.map(p =>
+      val emptyColumnsCount = emptyColums.count(_ < p.column)
+      p.copy(column = p.column + (emptyColumnsCount) * (times - 1))
+    )
+    updatedLinesAndColumns
+
   def main(args: Array[String]): Unit =
     val input = FileReader.readLines(index, 2023)
     val parsed = parse(input)
     println(parsed)
-    val expanded = expandSpace(parsed)
+    val expanded = expandSpace(parsed,2, input.head.length, input.length)
     println(expanded)
-    println(part1(expanded))
+    val expanded2 = expandSpace(parsed, 1000000, input.head.length, input.length)
+    println(expanded2)
+    println(compute(expanded))
+    println(compute(expanded2))
