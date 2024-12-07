e7d922a8da4b25dde57610d5b6a8b2eabac64e47
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2024-12-07 12:43:53 +0200

    Day 5 part 2

--- src/main/scala/y2024/Day_5.scala
+++ src/main/scala/y2024/Day_5.scala
@@ -27,18 +27,38 @@ object Day_5:
     Something(rules, lists)
 
   def part1(input: Something) =
-    val filtered = input.printed.filter(l =>
-      input.rules.forall(rule => {
-        if (l.contains(rule.x) && l.contains(rule.y))
-          l.indexOf(rule.x) < l.indexOf(rule.y)
-        else true
-      })
-    )
+    val filtered = input.printed.filter(l => input.rules.forall(rule => isInCorrectOrder(rule, l)))
     filtered
+
+  private def isInCorrectOrder(rule: Rule, l: List[Int]) = {
+    if (l.contains(rule.x) && l.contains(rule.y))
+      l.indexOf(rule.x) < l.indexOf(rule.y)
+    else true
+  }
+
+  private def setInCorrectOrder(rules: List[Rule], l: List[Int]) =
+    rules.foldLeft(l)((acc, rule) =>
+      if (!isInCorrectOrder(rule, acc)) {
+        val xIndex = acc.indexOf(rule.x)
+        val yIndex = acc.indexOf(rule.y)
+        acc.updated(xIndex, rule.y).updated(yIndex, rule.x)
+      } else acc
+    )
+
+  private def setInCorrectOrderRecursive(rules: List[Rule], l: List[Int]): List[Int] =
+    if (rules.forall(rule => isInCorrectOrder(rule, l))) l
+    else {
+      val firstRun = setInCorrectOrder(rules, l)
+      setInCorrectOrderRecursive(rules, firstRun)
+    }
 
   def part2(input: Something, correctLists: List[List[Int]]) =
     val wrongLists = input.printed.filter(l => !correctLists.contains(l))
-    println(wrongLists)
+    val corrected = wrongLists.map(l => setInCorrectOrderRecursive(input.rules, l))
+//    println(wrongLists)
+//    println(corrected)
+    corrected
+
   def main(args: Array[String]): Unit =
     val input = FileReader.readString(index, 2024)
     val parsed = parse(input)
@@ -46,4 +66,4 @@ object Day_5:
     val correctLists = part1(parsed)
     val resPart1 = correctLists.map(l => l(l.length / 2)).sum
     println(resPart1)
-    println(part2(parsed, correctLists))
+    println(part2(parsed, correctLists).map(l => l(l.length / 2)).sum)
