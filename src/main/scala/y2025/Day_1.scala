8e2cf9d91c29cba372591a696cca66f696b25631
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2025-12-02 01:34:44 +0200

    Day 1 part 2

--- src/main/scala/y2025/Day_1.scala
+++ src/main/scala/y2025/Day_1.scala
@@ -3,10 +3,6 @@ package y2025
 
 import util.FileReader
 
-import com.cannondev.advent.y2025.Day_1.Direction
-
-import scala.io.Source
-
 object Day_1:
   enum Direction {
     case Left
@@ -17,8 +13,6 @@ object Day_1:
 
   val index = 1
 
-
-
   def rotate(start: Int, rotations: List[Rotation]): Counter = {
     rotations.foldLeft(Counter(start, 0)) { case (acc, rotation) =>
       val rotated = rotation.dir match
@@ -26,11 +20,32 @@ object Day_1:
         case Direction.Right => acc.current + rotation.value
       val adjusted =
         if (rotated > 99) rotated % 100
-        else if (rotated < 0)  (100 + rotated % 100) % 100
+        else if (rotated < 0) (100 + rotated % 100) % 100
         else rotated
       val numberOfZeros = adjusted match
         case 0 => acc.numberOfZeros + 1
         case _ => acc.numberOfZeros
+      Counter(adjusted, numberOfZeros)
+    }
+  }
+
+  def rotateP2(start: Int, rotations: List[Rotation]): Counter = {
+    rotations.foldLeft(Counter(start, 0)) { case (acc, rotation) =>
+      val intermediaryZeroes = rotation.value / 100
+      val adjustedRotation = rotation.value  % 100
+      val rotated = rotation.dir match
+        case Direction.Left  => acc.current -adjustedRotation
+        case Direction.Right => acc.current +adjustedRotation
+      val adjusted =
+        if (rotated > 99) rotated % 100
+        else if (rotated < 0) (100 + rotated % 100) % 100
+        else rotated
+
+      val changedSign = if (adjusted != rotated && acc.current != 0 && rotated % 100 != 0) 1 else 0
+      val numberOfZeros = adjusted match
+        case 0 if adjustedRotation == 0 => acc.numberOfZeros + intermediaryZeroes + changedSign
+        case 0                              => acc.numberOfZeros + 1 + intermediaryZeroes + changedSign
+        case _                              => acc.numberOfZeros + intermediaryZeroes + changedSign
       Counter(adjusted, numberOfZeros)
     }
   }
@@ -45,10 +60,11 @@ object Day_1:
       Rotation(dir, value)
     })
 
-
   // Used: 893
+  // P2: 6163, 5531, 5978
   def main(args: Array[String]): Unit =
     val input = FileReader.readLines(index, 2025)
     val parsed = parse(input)
     println(parsed)
     println(rotate(50, parsed))
+    println(rotateP2(50, parsed))
